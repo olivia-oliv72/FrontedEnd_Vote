c7e9524ff546bba9a614fc5bfb5d42e42986a39f
OLIVIA OLIVIA <6182201006@student.unpar.ac.id>
2025-05-24 13:45:16 +0700

    sudah bisa login role user, direct ke halaman homePage untuk User

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import "../assets/css/navbar.css";
 import { getUser, getUserRole } from "../utils/authentication";
 import { Switch, Match } from "solid-js";
+import img_user from "../assets/img/logo_user.png"
 
 export default function NavbarGuest() {
   const user = getUser(); // null kalau belum login
@@ -20,7 +21,14 @@ export default function NavbarGuest() {
 
           <Match when={user?.role === "user"}>
             {/* User */}
-            
+            <div class="container-nav-user">
+              <a href="">Home</a>
+              <a href="">History</a>
+              <div class="user">
+                <span>Welcome, {user.username}!</span>
+                <img src={img_user} alt="image_user" />
+              </div>
+            </div>
           </Match>
 
           <Match when={user?.role === "admin"}>
