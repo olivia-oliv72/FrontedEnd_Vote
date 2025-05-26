e6159487cdfee817b91787b436bc4c14bb8ced25
unknown <elianalitaniii@gmail.com>
2025-05-26 20:53:52 +0700

    logout works

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./pages/guest/Login";
 import Voting from "./pages/voter/Voting";
 import Confirmation from "./pages/voter/Confirmation";
 import Profile from "./pages/voter/Profile";
+import Logout from "./pages/voter/Logout";
 
 import AdminDashboard from "./pages/admin/Dashboard";
 // import AddCategory from "./pages/admin/AddCategory";
@@ -18,6 +19,7 @@ function App() {
       {/* Halaman umum */}
       <Route path="/" component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/logout" component={Logout}/>
       <Route path="/voting/:categoryId" component={Voting} />
       <Route path="/confirmation/:categoryId" component={Confirmation} />
       <Route path="/admin" component={AdminDashboard} />
